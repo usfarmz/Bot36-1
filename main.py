04d256801dbdb1bf9555d51d82d8a2ce35e041d7
usfarmz <kevinbelameiri@gmail.com>
2025-11-22 04:45:58 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = "8532082529:AAHCiDhoHsPzp43m5tX4fPsZFFwRqeRTTAw"
+WEBAPP_URL = "https://clope36.42web.io/?i=1#"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [
+        [InlineKeyboardButton(text="Ouvrir la Mini-App 🚀", url=WEBAPP_URL)]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text(
+        "Bienvenue dans Clope36 Bot !\nAppuie sur le bouton ci-dessous 👇",
+        reply_markup=reply_markup
+    )
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
